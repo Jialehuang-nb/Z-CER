c653accf1cba4926f31bb1c7ccd445260fb1ed22
王 佳禾 <pia317@mail.ustc.edu.cn>
2024-03-20 10:37:36 +0800

    update vote

--- vote.py
+++ vote.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
-
 import torch.nn.functional as F
-
 import numpy as np
 from torchvision import models, transforms
 import torchvision.transforms as transforms
@@ -11,27 +9,28 @@ from collections import Counter
 from torch.utils.data import Dataset
 from PIL import Image
 from sklearn.metrics import accuracy_score, f1_score
-
 from torch.utils.tensorboard import SummaryWriter  
-
-
 import pandas as pd
-
-
-
 import  os
 from tqdm import tqdm
+import matplotlib.pyplot as pl
+from sklearn import metrics
+import torch
+from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score, f1_score
+from tqdm import tqdm
+from collections import Counter
+import numpy as np
 
 
-BATCHSIZE=32 # batchsize必须是testdata的整数倍
-EPOCHES=500
-LR=0.001
+BATCHSIZE=64 # batchsize必须是testdata的整数倍
+EPOCHES=2
+LR=0.0001
 numclass = 7
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("[INFO] Device: ", device)
 writer = SummaryWriter('output')
-
 
 
 def mbnet():
@@ -47,7 +46,6 @@ def mbnet():
 	model.classifier = fc
 	return model
 
-
 def mnasnet():
 	model = models.MNASNet(alpha=1)
 
@@ -59,49 +57,38 @@ def mnasnet():
 		nn.Linear(1280, numclass),
 	)
 	model.classifier = fc
-
 	return model
 
 def densenet121():
     model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
-
     for param in model.parameters():
         param.requires_grad = True
-
     classifier = nn.Sequential(
                         nn.Linear(1024, 500),
                         nn.ReLU(),
                         nn.Dropout(0.4),
                         nn.Linear(500, numclass)
                         )
-
     model.classifier = classifier
-
     return model
 
 def densenet201():
     model = models.densenet201(weights=models.DenseNet201_Weights.DEFAULT)
-
     for param in model.parameters():
         param.requires_grad = True
-
     classifier = nn.Sequential(
                         nn.Linear(1920, 500),
                         nn.ReLU(),
                         nn.Dropout(0.4),
                         nn.Linear(500, numclass)
                         )
-
     model.classifier = classifier
-
     return model
 
 def resnet18(fc_num=256, class_num=numclass):
 	model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-
 	for param in model.parameters():
 		param.requires_grad = True
-
 	fc_inputs = model.fc.in_features
 	model.fc = nn.Sequential(
 		nn.Linear(fc_inputs, fc_num),
@@ -109,17 +96,12 @@ def resnet18(fc_num=256, class_num=numclass):
 		nn.Dropout(0.4),
 		nn.Linear(fc_num, class_num)
 	)
- 
 	return model
-
 
 def resnet152(fc_num=256, class_num=numclass, train_all =False):
 	model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
-
 	for param in model.parameters():
 		param.requires_grad = True
-
-
 	fc_inputs = model.fc.in_features
 	model.fc = nn.Sequential(
 		nn.Linear(fc_inputs, fc_num),
@@ -127,22 +109,19 @@ def resnet152(fc_num=256, class_num=numclass, train_all =False):
 		nn.Dropout(0.4),
 		nn.Linear(fc_num, class_num)
 	)
-
 	if train_all:
 		for param in model.parameters():
 			param.requires_grad = False
 		torch.load("./models/best_loss.pt")
-
 	return model
 
 class dataload(Dataset):
     MEAN = [0.367035294117647, 0.41083294117647057, 0.5066129411764705]  
     STD = [1, 1, 1] 
-
-    def __init__(self, df, dataset_name, img_dir) -> None:
+    
+    def __init__(self, df, dataset_name) -> None:
         super(dataload).__init__()
         self.df = df
-        self.img_dir = img_dir
 
         if dataset_name in ['train']:
             self.preprocess = transforms.Compose([
@@ -164,38 +143,21 @@ class dataload(Dataset):
         
         img_name = self.df['img_name'].iloc[index]
         lable = self.df['label'].iloc[index]
-
         X = Image.open(img_name)
         X = self.preprocess(X)
-
         return X, lable
 
     def __len__(self):
         return len(self.df)
 
-import matplotlib.pyplot as pl
-from sklearn import metrics
-import torch
-from torch.utils.data import DataLoader
-from sklearn.metrics import accuracy_score, f1_score
-from tqdm import tqdm
-from collections import Counter
-import numpy as np
-
 
 def plot_matrix(y_true, y_pred, labels_name, save_dir, title=None, thresh=0.8, axis_labels=None):
-
     cm = metrics.confusion_matrix(y_true, y_pred, labels=labels_name, sample_weight=None) 
     cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]  
-
-
     pl.imshow(cm, interpolation='nearest', cmap=pl.get_cmap('Blues'))
     pl.colorbar()  
-
-
     if title is not None:
         pl.title(title)
-
     num_local = np.array(range(len(labels_name)))
     if axis_labels is None:
         axis_labels = labels_name
@@ -215,25 +177,21 @@ def plot_matrix(y_true, y_pred, labels_name, save_dir, title=None, thresh=0.8, a
     pl.clf()
     pl.cla()
 
-
 def data_process(batch_size=32, abawce = False):
-	
     df_train = pd.read_csv('train.csv')
     df_valid = pd.read_csv('test.csv')
     df_valid = df_valid.sample(int(len(df_valid)/BATCHSIZE)*BATCHSIZE)
-
     label_conuts = df_train['label'].value_counts()
     print(label_conuts)
+    
     train_set = dataload(df_train,'train')
     valid_set = dataload(df_valid,'valid')
-
     batch_size = BATCHSIZE
     train_data = DataLoader(train_set, batch_size, shuffle=True, num_workers=8)
     valid_data = DataLoader(valid_set, batch_size, shuffle=False, num_workers=8)
-
     train_data_size = len(df_train)
     valid_data_size = len(df_valid)
-
+    
     print("[INFO] Train data / Test data number: ", train_data_size, valid_data_size)
     return train_data, valid_data, label_conuts
 
@@ -294,7 +252,7 @@ def process(mlps, trainloader, testloader, label_conuts):
 
         mlps_pred_train = [[] for _ in range(len(mlps))]
         label_train = []
-        for img, label, _ in tqdm(trainloader):
+        for img, label in tqdm(trainloader):
             img, label = img.to(device), label.to(device)
             label_one_hot = F.one_hot(label, num_classes=7).float()
             label_train.append(label)
@@ -335,7 +293,7 @@ def process(mlps, trainloader, testloader, label_conuts):
         
         print("[INFO] Begin to valid")
         with torch.no_grad():
-            for img, label, name in tqdm(testloader):
+            for img, label in tqdm(testloader):
                 img = img.to(device)
                 label_valid.append(label)
                 for i, mlp in enumerate(mlps):
@@ -365,7 +323,7 @@ def process(mlps, trainloader, testloader, label_conuts):
 
         if vote_f1 > best_f1:
             for i, mlp in enumerate(mlps):
-                torch.save(mlp.state_dict(), 'model/model'+str(i)+'.pth')
+                torch.save(mlp.state_dict(), 'vote_model_state/'+str(i)+'.pth')
 
         print("epoch:" + str(ep + 1) + "\n集成模型的acc=" + str(vote_acc) + ", f1=" + str(vote_f1))
         for idx, mlp_valid in enumerate(mlps_pred_valid):
